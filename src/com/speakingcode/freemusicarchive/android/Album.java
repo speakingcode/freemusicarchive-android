4e0adec6edd2e86f8cc4786361da3de9d46d161b
Daneil Lissner <dlissner@gmail.com>
2012-09-08 16:40:50 -0500

    finished genres and added albums

---
+++ src/com/speakingcode/freemusicarchive/android/Album.java
@@ -0,0 +1,127 @@
+package com.speakingcode.freemusicarchive.android;
+
+public class Album
+{
+	String albumId;
+	String albumTitle;	
+	String albumHandle;
+	String albumType;
+	String albumUrl;
+	String artistName;
+	String artistUrl;
+	String albumDateReleased;	
+	String albumDateCreated;
+	String albumListens;
+	String albumPublisher;
+	
+	
+	public String getAlbumId()
+	{
+		return albumId;
+	}
+	
+	public void setAlbumId(String albumId)
+	{
+		this.albumId = albumId;
+	}
+	
+	public String getAlbumTitle()
+	{
+		return albumTitle;
+	}
+	
+	public void setAlbumTitle(String albumTitle)
+	{
+		this.albumTitle = albumTitle;
+	}
+	
+	public String getAlbumHandle()
+	{
+		return albumHandle;
+	}
+	
+	public void setAlbumHandle(String albumHandle)
+	{
+		this.albumHandle = albumHandle;
+	}
+	
+	public String getAlbumType()
+	{
+		return albumType;
+	}
+	
+	public void setAlbumType(String albumType)
+	{
+		this.albumType = albumType;
+	}
+	
+	public String getAlbumUrl()
+	{
+		return albumUrl;
+	}
+	
+	public void setAlbumUrl(String albumUrl)
+	{
+		this.albumUrl = albumUrl;
+	}
+	
+	public String getArtistName()
+	{
+		return artistName;
+	}
+	
+	public void setArtistName(String artistName)
+	{
+		this.artistName = artistName;
+	}
+	
+	public String getArtistUrl()
+	{
+		return artistUrl;
+	}
+	
+	public void setArtistUrl(String artistUrl)
+	{
+		this.artistUrl = artistUrl;
+	}
+	
+	public String getAlbumDateReleased()
+	{
+		return albumDateReleased;
+	}
+	
+	public void setAlbumDateReleased(String albumDateReleased)
+	{
+		this.albumDateReleased = albumDateReleased;
+	}
+	
+	public String getAlbumDateCreated()
+	{
+		return albumDateCreated;
+	}
+	
+	public void setAlbumDateCreated(String albumDateCreated)
+	{
+		this.albumDateCreated = albumDateCreated;
+	}
+	
+	public String getAlbumListens()
+	{
+		return albumListens;
+	}
+	
+	public void setAlbumListens(String albumListens)
+	{
+		this.albumListens = albumListens;
+	}
+	
+	public String getAlbumPublisher()
+	{
+		return albumPublisher;
+	}
+	
+	public void setAlbumPublisher(String albumPublisher)
+	{
+		this.albumPublisher = albumPublisher;
+	}
+}
---
+++ src/com/speakingcode/freemusicarchive/android/AlbumRecordSet.java
@@ -0,0 +1,87 @@
+package com.speakingcode.freemusicarchive.android;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AlbumRecordSet
+{
+	ArrayList<Album> albumRecords;
+	int total;
+	int totalPages;
+	int currentPage;
+	
+	public AlbumRecordSet(ArrayList<Album> records, int total, int totalPages, int currentPage)
+	{
+		this.albumRecords	= records;
+		this.total			= total;
+		this.totalPages		= totalPages;
+		this.currentPage	= currentPage;
+	}
+	
+	public int getTotal()
+	{
+		return total;
+	}
+
+	public void setTotal(int total)
+	{
+		this.total = total;
+	}
+
+	public int getTotalPages()
+	{
+		return totalPages;
+	}
+
+	public void setTotalPages(int totalPages)
+	{
+		this.totalPages = totalPages;
+	}
+
+	
+	public int getCurrentPage()
+	{
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage)
+	{
+		this.currentPage = currentPage;
+	}
+
+	public ArrayList<Album> getAlbumRecords()
+	{
+		return albumRecords;
+	}
+	
+	public void setAlbumRecords(ArrayList<Album> records)
+	{
+		this.albumRecords = records;
+	}
+	
+	public void addAlbums(ArrayList<Album> newAlbums)
+	{
+		this.albumRecords.addAll(newAlbums);
+	}
+	
+	public void addAlbum(Album newAlbum)
+	{
+		this.albumRecords.add(newAlbum);
+	}
+	
+	public void removeAlbum(Album albumToRemove)
+	{
+		this.albumRecords.remove(albumToRemove);
+	}
+	
+	public void removeAlbum(String removeAlbumId)
+	{
+		Iterator<Album> i = albumRecords.iterator();
+		while (i.hasNext())
+		{
+			if (i.next().getAlbumId() == removeAlbumId)
+				i.remove();
+		}
+	}
+
+}
